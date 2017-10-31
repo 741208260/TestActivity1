e9d57c84645ec5564be88be539f30f029def018b
741208260 <741208260@qq.com>
2017-10-31 08:26:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/testactivity/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.administrator.testactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_skip;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        btn_skip = (Button) findViewById(R.id.heihie);
+        Log.e(TAG, this.toString());
+        btn_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, this.toString());
+                Log.e(TAG, MainActivity.this.toString());
+                Intent intent = new Intent("gaoyu521");
+
+                /*Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));*/
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * 创建菜单
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        Toast.makeText(this, "删除嘿嘿嘿么么哒吗", Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_add:
+                Toast.makeText(this, "增加", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_remove:
+                Toast.makeText(this, "删除嘿嘿嘿么么哒吗", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
